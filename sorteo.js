b0081cc80a54a30f440e5bb3380b64055ed1e574
Leonardo <martinezjleonardo@gmail.com>
2024-07-27 18:08:32 -0300

    2do

--- sorteo.js
+++ sorteo.js
@@ -5,6 +5,7 @@ const jsConfetti = new JSConfetti()
 const start = document.querySelector(".start")
 const reiniciar = document.querySelector(".reiniciar")
 const ultimoNumero = document.querySelector(".numeroReciente")
+const jugados = document.querySelector(".jugados")
 let numerosJugados = []
 
 
@@ -12,11 +13,17 @@ start.addEventListener("click", ()=>{
     let max = 90
     let min = 1
     let congrats = false
+    
     if (numerosJugados.length == 0){
         let numeroElegido = Math.floor(Math.random() * (max - min + 1)) + min;
         ultimoNumero.textContent = numeroElegido
         numerosJugados.push(numeroElegido)
     }else{
+        
+        
+        jugados.innerHTML = `Jugados: ${numerosJugados}` 
+        
+
         numeroElegido = Math.floor(Math.random() * (max - min + 1)) + min;
         if(!numerosJugados.includes(numeroElegido)){
             numerosJugados.push(numeroElegido)
@@ -42,9 +49,11 @@ start.addEventListener("click", ()=>{
             }
         }
 
+        
     }
-})
 
+    
+    })
 
 
 reiniciar.addEventListener("click",()=>{
@@ -52,5 +61,6 @@ reiniciar.addEventListener("click",()=>{
     numerosJugados = []
     ultimoNumero.textContent = ""
     console.log(numerosJugados)
+    jugados.textContent =""
     
 })
